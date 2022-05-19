0b098712d618fd4aabc5db855e6f2cc69c26c1ef
Rishi Kumar <24rkumar@student.dasd.org>
2022-05-18 21:46:27 -0400

    Added keyboard

---
+++ src/main/kotlin/Keyboard.kt
@@ -0,0 +1,56 @@
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Keyboard(colors: Map<Char, Color>, enter: () -> Unit, action: (Char) -> Unit) {
+    val chars = listOf("qwertyuiop", "asdfghjkl")
+    val lastRow = "zxcvbnm"
+
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        chars.forEach { rowChars ->
+            Row(
+                modifier = Modifier.weight(1f).align(Alignment.CenterHorizontally)
+            ) {
+                rowChars.forEach {
+                    KeyboardKey(it, colors[it] ?: Color.Gray, action)
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier.weight(1f).align(Alignment.CenterHorizontally)
+        ) {
+            lastRow.forEach {
+                KeyboardKey(it, colors[it] ?: Color.Gray, action)
+            }
+
+            Button(
+                onClick = { enter() },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray),
+                modifier = Modifier.fillMaxHeight().width(90.dp).padding(1.dp)
+            ) {
+                Text("Enter")
+            }
+        }
+    }
+}
+
+@Composable
+fun KeyboardKey(c: Char, color: Color, action: (Char) -> Unit) {
+    Button(
+        onClick = { action(c) },
+        colors = ButtonDefaults.buttonColors(backgroundColor = color),
+        modifier = Modifier.fillMaxHeight().width(60.dp).padding(1.dp)
+    ) {
+        Text(c.toString())
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 fun main() = Window {
+    val colors = mapOf<Char, Color>()
+    var text by remember { mutableStateOf("") }
 
+    Column {
+        Text(text, modifier = Modifier.weight(6f), color = Color.Black)
+        Row(modifier = Modifier.weight(1f)) {}
+        Row(
+            modifier = Modifier.weight(3f)
+        ) {
+            Keyboard(colors, enter = { text = "" }) { text += it.toString() }
+        }
+    }
 }
